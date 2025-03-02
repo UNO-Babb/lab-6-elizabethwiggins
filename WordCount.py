713b18d7bffc56d2f8220ac2961a2fa060bf6aeb
elizabethwiggins <ewiggins@unomaha.edu>
2025-03-02 02:56:29 +0000

    finished word count

--- WordCount.py
+++ WordCount.py
@@ -4,11 +4,25 @@
 #Assignment:
 
 def main():
-  textFile = open("gettysberg.txt", 'r')
-  
+  textFile = open("fish.txt", 'r')
+  lineCount= 0
+  wordCount= 0
+  letterCount= 0
+
   for line in textFile:
-    print(line)
-  
+    lineCount= lineCount+1
+    words= line.split()
+    #print(words)
+    for w in words:
+      wordCount= wordCount+1
+      for char in words:
+        letterCount=letterCount+1
+      
+    #print(line)
+
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Characters:", letterCount)
 
 if __name__ == '__main__':
   main()
